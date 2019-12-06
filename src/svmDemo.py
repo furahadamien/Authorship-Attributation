d15713183a1aa41e30cf8ac8006198c15dbabed8
Furaha Damién <furaha.damien@mail.mcgill.ca>
2019-12-06 18:46:34 -0500

    fused code

--- src/svmDemo.py
+++ src/svmDemo.py
@@ -20,6 +20,12 @@ from collections import defaultdict
 from sklearn import model_selection, naive_bayes, svm
 from sklearn.linear_model import SGDClassifier
 import string
+import math
+
+from sklearn.model_selection import train_test_split
+
+tfidf_vect= TfidfVectorizer(  use_idf=True, smooth_idf=True, sublinear_tf=False)
+
 
 categories = ["candidate00001", "candidate00002", "candidate00003",
 "candidate00004", "candidate00005", "candidate00006", "candidate00007",
@@ -62,6 +68,40 @@ for x in range (0, len(test_doc)):
     #print(nltk.pos_tag(token))
 count_vect = CountVectorizer()
 
+gamma = 0.1
+
+def Dd(P, Q):
+    s = 0
+    if len(P) != len(Q):
+        return 0
+    
+    for i in range(1, len(P) + 1):
+        s += math.acos(math.sqrt(P[i]) * math.sqrt(Q[i]))
+    return s
+
+def D(P, Q):
+    s = 0
+    if len(P) != len(Q):
+        return 0
+    for i in range(0, len(P)):
+        s += (P[i] - Q[i])*(P[i] - Q[i])
+
+    return math.sqrt(s)
+
+def K(P, Q):
+    return math.exp(-1 * math.pow(D(P,Q),2) / gamma)
+
+# for LOWBOW: each row of P contains the unweighted sum over BOLH
+def my_kernel(A, B):
+    matrix = [[0 for i in range(len(B))] for j in range(len(A)) ]
+    for i in range(len(A)):
+        for j in range(len(B)):
+            matrix[i][j] = K(A[i], B[j])
+    return matrix
+
+
+h = .02  # step size in the mesh
+
 #stop_words = stopwords.words('english')
 #stop_words = set(stopwords.words('english'))
 #print(string.punctuation)
@@ -74,19 +114,32 @@ X_train_tf = tf_transformer.transform(X_train_counts)
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
+
+df= pd.DataFrame({'text':test_doc, 'class': test_data.target})
+
+X = tfidf_vect.fit_transform(df['text'].values)
+y = df['class'].values
+
+from sklearn.decomposition.truncated_svd import TruncatedSVD 
+pca = TruncatedSVD(n_components=2)                                
+X_reduced_train = pca.fit_transform(X)
+a_train, a_test, b_train, b_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+from sklearn.ensemble import RandomForestClassifier 
+classifier=RandomForestClassifier(n_estimators=10)                  
+classifier.fit(a_train.toarray(), b_train)                            
+prediction = classifier.predict(a_test.toarray()) 
+
+clf = svm.SVC(kernel=my_kernel)
 # Support Vector Machine model
-text_clf = Pipeline([('vect', CountVectorizer()),
-('tfidf', TfidfTransformer()),
-('clf', SGDClassifier(loss='hinge', penalty='l2',
-alpha=1e-3, random_state=42,
-max_iter=5, tol=None)),])
-text_clf.fit(train_data.data, train_data.target)
+#text_clf = Pipeline([('vect', CountVectorizer()),
+#(#'tfidf', TfidfTransformer()),
+#('clf', clf),])
+
+clf.fit(a_train.toarray(), b_train)
 
 #SVM evaluation
-predicted = text_clf.predict(test_doc)
-print('Support Vector Machine accuracy %r:' %np.mean(predicted == test_data.target) )
+#predicted = clf.predict(test_doc)
+print('Support Vector Machine accuracy %r:' %np.mean(prediction == b_train) )
 print('Support Vector Machin model confusion Matrix')
-print(metrics.confusion_matrix(test_data.target, predicted))
-
-
-
+print(metrics.confusion_matrix(b_train, prediction))
